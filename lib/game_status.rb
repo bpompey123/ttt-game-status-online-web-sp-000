78ec47d78981c7f5e76a14279c9df6fdb6587fa3
Brian Pompey <brianthescholar@gmail.com>
2019-07-16 22:26:26 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -17,6 +17,9 @@ WIN_COMBINATIONS = [
 
 def won?(board)
   WIN_COMBINATIONS.select do |win_combination|
+    win_index_1 = win_combination[0]
+    win_index_2 = win_combination[1]
+    win_index_3 = win_combination[2]
     position_taken?(board, index) == win_combination
   end
 end
