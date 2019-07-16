a8818d149218f5da24ddcca45835d57f6c20031b
Brian Pompey <brianthescholar@gmail.com>
2019-07-16 22:27:58 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -24,7 +24,7 @@ def won?(board)
     position_1 = board[win_index_1]
     position_2 = board[win_index_2]
     position_3 = board[win_index_3]
-    
+
     position_taken?(board, win_index_1) == win_combination
   end
 end
